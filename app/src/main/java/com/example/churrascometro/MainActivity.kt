7767ec70fd89897a1ca8709647aee45017987c30
Carlos Mendes <carlosmendesjmc@gmail.com>
2026-07-14 17:24:52 +0100

    feat: implement navigation flow between activities

--- app/src/main/java/com/example/churrascometro/MainActivity.kt
+++ app/src/main/java/com/example/churrascometro/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.churrascometro
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Recupera o botão
+        val Iniciar = findViewById<Button>(R.id.btn_IniciarMainActivity)
+        //Ação do botão
+        Iniciar.setOnClickListener {
+            intent = Intent(this, AdultsActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
