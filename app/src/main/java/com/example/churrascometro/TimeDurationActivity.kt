6ec6bc3abf973d6dcd6b35a2001904d248c7b107
Carlos Mendes <carlosmendesjmc@gmail.com>
2026-07-15 23:19:56 +0100

    feat: implement toolbar navigation across activities

--- app/src/main/java/com/example/churrascometro/TimeDurationActivity.kt
+++ app/src/main/java/com/example/churrascometro/TimeDurationActivity.kt
@@ -2,11 +2,13 @@ package com.example.churrascometro
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class TimeDurationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +20,29 @@ class TimeDurationActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Recupera toolbar
+        val toolbar = findViewById<MaterialToolbar>(R.id.materialToolbarTimeDuration)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         //Recupera o botão
-        val Iniciar = findViewById<Button>(R.id.btn_ProximoTimeDurationActivity)
+        val proximo = findViewById<Button>(R.id.btn_ProximoTimeDurationActivity)
         //Ação do botão
-        Iniciar.setOnClickListener {
+        proximo.setOnClickListener {
             intent = Intent(this, ResultActivity::class.java)
             startActivity(intent)
         }
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home->{
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
